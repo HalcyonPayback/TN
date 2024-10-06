046decc099d0be5e93df052ee0913b3432ed3f0a
HalcyonPayback <christopererika@gmail.com>
2024-10-06 22:42:00 +0800

    Update PreOp.lua

--- PreOp.lua
+++ PreOp.lua
@@ -1,3 +1,5 @@
 --You can use loadstring to do this, It must be enabled via ServerScriptService tho!
 
-EDIT: Then you would do loadstring(“https://github.com/Roblox/rodux/something.lua 211”)
+--EDIT: Then you would do 
+
+loadstring(“”)
