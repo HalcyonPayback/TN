bb38054a6845031c6648f28ea2cdc23a98895b24
HalcyonPayback <christopererika@gmail.com>
2024-10-06 22:58:57 +0800

    Update PreOp.lua

--- PreOp.lua
+++ PreOp.lua
@@ -1,5 +1,3 @@
 --You can use loadstring to do this, It must be enabled via ServerScriptService tho!
-
 --EDIT: Then you would do 
-
-loadstring(“https://github.com/HalcyonPayback/TN/raw/refs/heads/main/Prg1.lua”)
+loadstring("https://github.com/HalcyonPayback/TN/raw/refs/heads/main/Prg1.lua")
