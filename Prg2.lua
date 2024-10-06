9519b298e75cbf7d0e76d5708f9564b68c333f90
HalcyonPayback <christopererika@gmail.com>
2024-10-06 22:57:03 +0800

    Update Prg2.lua

--- Prg2.lua
+++ Prg2.lua
@@ -1,6 +1,1 @@
---[[A = {149484300}
-B = {}
-if table.find(A,game.CreatorId)==nil or table.find(B,game.CreatorId) ~= nil or game:GetService("ServerStorage"):FindFirstChild("StorCache") == nil then
-  
-else
-end]]
+
